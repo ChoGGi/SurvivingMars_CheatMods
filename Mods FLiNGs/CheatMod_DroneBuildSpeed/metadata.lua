e184c6b327908e19b3a8cbeacc44b7a8fb79732c
ChoGGi <github@choggi.org>
2018-03-24 07:38:58 -0600

    Delete metadata.lua

--- Mods FLiNGs/CheatMod_DroneBuildSpeed/metadata.lua
+++
@@ -1,9 +0,0 @@
-return PlaceObj('ModDef', {
-	'title', "CheatMod: Drone build speed",
-	'description', "Instant build/repair when resources are ready.",
-	'tags', "Cheats",
-	'id', "NgFmLhn",
-	'author', "FLiNG",
-	'version', 5,
-	'saved', 1521220801,
-})
