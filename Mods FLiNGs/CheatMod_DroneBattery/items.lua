0570b01cbf07f07686e0513a58bd141ccfc6daa3
ChoGGi <github@choggi.org>
2018-03-24 07:39:08 -0600

    Delete items.lua

--- Mods FLiNGs/CheatMod_DroneBattery/items.lua
+++
@@ -1,6 +0,0 @@
-Consts.DroneMoveBatteryUse = 0
-Consts.DroneCarryBatteryUse = 0
-Consts.DroneConstructBatteryUse = 0
-Consts.DroneBuildingRepairBatteryUse = 0
-Consts.DroneDeconstructBatteryUse = 0
-Consts.DroneTransformWasteRockObstructorToStockpileBatteryUse = 0
