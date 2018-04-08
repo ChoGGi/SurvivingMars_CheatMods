d49ccd0619edb98e70a81a395b2bee9fd4518a3f
ChoGGi <github@choggi.org>
2018-04-08 14:08:33 -0600

    CheatMod_WorkingMysteryMenus
    Requested mod: Adds the fixed start mystery menu items.

---
+++ Mods ChoGGi/CheatMod_WorkingMysteryMenus/items.lua
@@ -0,0 +1,6 @@
+return {
+  PlaceObj('ModItemCode', {
+    'name', "WorkingMysteryMenus",
+    'FileName', "Script.lua",
+  }),
+}
---
+++ Mods ChoGGi/CheatMod_WorkingMysteryMenus/metadata.lua
@@ -0,0 +1,11 @@
+return PlaceObj("ModDef", {
+	"title", "CheatMod: Working Mystery Menus",
+	"description", "The Mystery items in the original cheat menu are broken. This fixes them.",
+	"tags", "Cheats",
+	"id", "ChoGGi_WorkingMysteryMenus",
+	"author", "ChoGGi",
+	"version", 1,
+	"code", {
+		"Script.lua",
+	},
+})
