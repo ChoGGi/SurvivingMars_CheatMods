a4670f5df87d15a2b5c6a813929000946d4e49b5
ChoGGi <github@choggi.org>
2018-04-08 22:50:44 -0600

    CheatMod_CheatMenu
    Bumped version

--- CheatMod_CheatMenu/metadata.lua
+++ CheatMod_CheatMenu/metadata.lua
@@ -19,7 +19,7 @@ return PlaceObj("ModDef", {
   "id", "ChoGGi_CheatMenu",
   --"steam_id","",
 	"author", "ChoGGi (thanks to chippydip, therealshibe, BoehserOnkel, Fling)",
-	"version", 2.1,
+	"version", 2.2,
   --"lua_revision","",
 	"code", {
 		"Init.lua",
