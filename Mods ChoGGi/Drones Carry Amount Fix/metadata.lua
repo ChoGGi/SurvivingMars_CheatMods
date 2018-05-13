2eda032c0ab9c3611637be43688329e2a4619a3e
ChoGGi <SurvivingMars_CheatMods@choggi.org>
2018-05-12 23:43:32 -0600

    Bumped versions
    Signed-off-by: ChoGGi <SurvivingMars_CheatMods@choggi.org>

--- Mods ChoGGi/Drones Carry Amount Fix/metadata.lua
+++ Mods ChoGGi/Drones Carry Amount Fix/metadata.lua
@@ -1,7 +1,7 @@
 return PlaceObj("ModDef", {
-  "saved", 1525780800,
-  "version", 1,
-  "title", "Drones Carry Amount Fix",
+	"title", "Drones Carry Amount Fix v0.2",
+	"version", 2,
+	"saved", 1526126400,
   "description", "Drones only pick up resources from buildings when the amount stored is equal or greater compared to their carry amount (this forces them to pick up whenever there's more then one resource).",
   "tags", "Building",
   "id", "ChoGGi_DronesCarryAmountFix",
