a0a1b2eb3a28e0999e0ffd8df803823a04855257
ChoGGi <SurvivingMars_CheatMods@choggi.org>
2018-05-15 15:43:14 -0600

    Signed-off-by: ChoGGi <SurvivingMars_CheatMods@choggi.org>

--- Mods ChoGGi/Deep Resources Never Run Out/Script.lua
+++ Mods ChoGGi/Deep Resources Never Run Out/Script.lua
@@ -1,18 +1,19 @@
+--depth_layer: 2 = deep, 1 = underground
 local function LargerDeposits(Class)
   local objs = GetObjects({class = Class}) or empty_table
   for i = 1, #objs do
-    if objs[i].depth_layer == 2 then --2 = deep, 1 = underground
+    if objs[i].depth_layer == 2 then --to apply to all deposits: remove this line
       objs[i].max_amount = 500000 * const.ResourceScale
-    end
+    end -- and this
   end
 end
 
 local function RefillAllDeposits(Class)
   local objs = GetObjects({class = Class}) or empty_table
   for i = 1, #objs do
-    if objs[i].depth_layer == 2 then
+    if objs[i].depth_layer == 2 then --to apply to all deposits: remove this line
       objs[i]:CheatRefill()
-    end
+    end -- and this
   end
 end
 
--- Mods ChoGGi/Deep Resources Never Run Out/metadata.lua
+++ Mods ChoGGi/Deep Resources Never Run Out/metadata.lua
@@ -2,7 +2,7 @@ return PlaceObj("ModDef", {
   "saved", 1526385600,
   "version", 1,
   "title", "Deep Resources Never Run Out",
-  "description", "Deep resources amounts are all set to 500k and get refilled every new Sol.",
+  "description", "Deep resources amounts are all set to 500k and get refilled every new Sol.\n\nChange depth_layer to 1 for underground deposits (or just remove the if for both)\n\nRequested by Mephane\nhttps://steamcommunity.com/workshop/discussions/18446744073709551615/3211505894135785138/",
   "tags", "Cheats",
   "id", "ChoGGi_DeepResourcesNeverRunOut",
   "author", "ChoGGi",
