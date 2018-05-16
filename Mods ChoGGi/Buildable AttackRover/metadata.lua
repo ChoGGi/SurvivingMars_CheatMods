3eead56dc3dc0b53554ec310340fcbd017b7d212
ChoGGi <SurvivingMars_CheatMods@choggi.org>
2018-05-15 22:35:24 -0600

    Signed-off-by: ChoGGi <SurvivingMars_CheatMods@choggi.org>

--- Mods ChoGGi/Buildable AttackRover/metadata.lua
+++ Mods ChoGGi/Buildable AttackRover/metadata.lua
@@ -1,7 +1,7 @@
 return PlaceObj("ModDef", {
   "saved", 1526385600,
   "version", 1,
-  "title", "Buildable AttackRover",
+  "title", "Buildable/Controllable AttackRover",
   "description", "It doesn't do anything, that's your job.",
   "tags", "Buildings",
   "id", "ChoGGi_BuildableAttackRover",
