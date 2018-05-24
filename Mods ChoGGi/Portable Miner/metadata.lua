4feb5253ff438626b5913e5736c30752703f35a0
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-05-24 10:00:38 -0600

    Portable Miner
    Engrish
    
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Portable Miner/metadata.lua
+++ Mods ChoGGi/Portable Miner/metadata.lua
@@ -2,7 +2,7 @@ return PlaceObj("ModDef", {
   "saved", 1527163200,
   "version", 1,
   "title", "Potable Miner",
-  "description", "Build it like rover then tell it where to go and if there's a resource (Metals/Concrete) close by it'll start mining it.\n\nUses Attack Rover model, the ground for concrete mined won't change.",
+  "description", "It's a rover that mines, tell it where to go and if there's a resource (Metals/Concrete) close by it'll start mining it.\n\nUses Attack Rover model, the ground where concrete is mined won't change.",
 	"image", "Preview.png",
   "tags", "Buildings",
   "id", "ChoGGi_PortableMiner",
