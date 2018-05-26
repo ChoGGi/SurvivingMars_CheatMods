0102ef7bfa8d408196fad61dfb1eada4dcc6c9a6
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-05-26 01:01:19 -0600

    Sample Sidepanel Controls: more samples
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Sample Sidepanel Controls/Script.lua
+++ Mods ChoGGi/Sample Sidepanel Controls/Script.lua
@@ -4,37 +4,36 @@ function OnMsg.ClassesBuilt()
   local ObjModified = ObjModified
   local PlaceObj = PlaceObj
 
-  --check if it was already added
-  if not XTemplates.ipResourceOverview.SOMETHINGUNIQUE then
-    XTemplates.ipResourceOverview.SOMETHINGUNIQUE = true
+  --check if the buttons were already added (you can have one for each, but meh)
+  if not XTemplates.sectionWorkplace.SOMETHINGUNIQUE then
+    XTemplates.sectionWorkplace.SOMETHINGUNIQUE = true
 
-    --this adds a button that changes depending on Object.working (a building that works...)
-    XTemplates.ipResourceOverview[#XTemplates.ipResourceOverview+1] = PlaceObj("XTemplateTemplate", {
+    --this adds a button to all workplaces that changes depending on Object.working (a building that works...)
+    XTemplates.sectionWorkplace[#XTemplates.sectionWorkplace+1] = PlaceObj("XTemplateTemplate", {
     --use OpenExamine(XTemplates) to see the complete list (and check that yours was added).
     --and https://github.com/HaemimontGames/SurvivingMars/tree/master/Lua/XTemplates
 
-      --added to the resource overview panel
-      "__context_of_kind", "ResourceOverview",
-      --?
-      "__template", "InfopanelActiveSection",
+      --added to the workplace panel, for more see OpenExamine(somebuilding) > click the class at the top till you get to metatable Building > then either __ancestors or __parents
+      "__context_of_kind", "Workplace",
+      "__template", "InfopanelSection",
 
       --you can set these here or in OnContextUpdate below
       "Icon", " ",
       "Title", " ",
       "RolloverText", " ",
 
-      "RolloverTitle", "Telepresence",
+      "RolloverTitle", "Hello, this needs to have something here for the hint to showup",
       "RolloverHint",  "",
       "OnContextUpdate", function(self, context)
         -- context is the object selected
         if context.working then
           self:SetRolloverText("This building is working.")
           self:SetTitle("Working")
-          self:SetIcon("UI/Icons/Upgrades/factory_ai_03.tga")
+          self:SetIcon("UI/Icons/Upgrades/factory_ai_01.tga")
         else
           self:SetRolloverText("This building isn't working.")
           self:SetTitle("Not Working")
-          self:SetIcon("UI/Icons/Upgrades/factory_ai_01.tga")
+          self:SetIcon("UI/Icons/Upgrades/factory_ai_03.tga")
         end
         ---
       end,
@@ -63,21 +62,48 @@ function OnMsg.ClassesBuilt()
     XTemplates.sectionWorkplace[#XTemplates.sectionWorkplace+1] = PlaceObj("XTemplateTemplate", {
       "__context_of_kind", "Workplace",
       "__template", "InfopanelSection",
-      "RolloverText", "",
+      "RolloverText", "Look ma it slides!",
       "RolloverHintGamepad", "",
+      "RolloverTitle", " ",
       "Title", " ", --updated below, can't be blank
       "Icon", "UI/Icons/Sections/facility.tga",
     }, {
       PlaceObj("XTemplateTemplate", {
         "__template", "InfopanelSlider",
         "BindTo", "max_workers",
-				'StepSize', 5, --change 5 per movement
+        --not sure how to limit amount?
+				"StepSize", 5, --change 5 per movement
         "OnContextUpdate", function(self, context)
           self.parent.parent:SetTitle("Change \"max_workers\" limit: " .. context.max_workers)
         end
       })
     })
 
-  end
+    --this adds a button to the resource overview that only shows when hour is over 10
+    --it needs the [1] or it takes over the whole screen (it's only needed for ipResourceOverview not the section ones)
+    XTemplates.ipResourceOverview[1][#XTemplates.ipResourceOverview[1]+1] = PlaceObj("XTemplateTemplate", {
+
+      --added to the resource overview panel
+      "__context_of_kind", "ResourceOverview",
+      "__template", "InfopanelActiveSection",
+
+      --you can set these here or in OnContextUpdate below
+      "Icon", "UI/Icons/Upgrades/factory_ai_03.tga",
+      "Title", "hour over 10",
+      "RolloverText", "only shows when hour is 10 or over",
+      "RolloverTitle", " ",
+      "RolloverHint",  "",
+      "OnContextUpdate", function(self, context)
+        if UICity.hour >= 10 then
+          self:SetVisible(true)
+          self:SetMaxHeight()
+        else
+          self:SetVisible(false)
+          self:SetMaxHeight(0)
+        end
+      end
+    })
+
+  end --if
 
 end
