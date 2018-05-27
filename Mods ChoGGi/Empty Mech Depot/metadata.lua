7c855688e557cffd0cbfc73189bd391535950f64
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-05-27 14:48:19 -0600

    Force Show Mod Manager Linux
    This is for Linux/OSX users not using the Steam version, since SM devs didn't add the best condition check...
    
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Empty Mech Depot/metadata.lua
+++ Mods ChoGGi/Empty Mech Depot/metadata.lua
@@ -1,7 +1,7 @@
 return PlaceObj("ModDef", {
   "saved", 1527336000,
-  "version", 2,
-  "title", "Empty Mech Depot v0.2",
+  "version", 3,
+  "title", "Empty Mech Depot v0.3",
   "description", "Empties out mech depot into a small depot in front of it (button added to side panel).",
   "tags", "Building",
   "id", "ChoGGi_EmptyMechDepot",
---
+++ Mods ChoGGi/Force Show Mod Manager Linux/BinAssets/AssetsRevision.lua
@@ -0,0 +1,12 @@
+function OnMsg.Autorun()
+
+  --if the menu order gets changed this won't work
+  XTemplates.PGMenu[1][2][3][5].__condition = function(parent, context)
+    --return Platform.steam or Platform.pc
+    return true
+  end
+
+end
+
+--return revision, or else you get a blank map on new game
+return 18637
