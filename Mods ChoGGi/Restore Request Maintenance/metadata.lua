c4ec14ecce2d86728e7c8d8dd9f1fa111c41937f
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-05-31 14:22:56 -0600

    Restore Request Maintenance button
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Restore Request Maintenance/metadata.lua
@@ -0,0 +1,11 @@
+return PlaceObj("ModDef", {
+  "saved", 1527768000,
+  "version", 1,
+  "title", "Restore Request Maintenance v0.1",
+  "description", "Restores \"Request Maintenance\" button.",
+  "id", "ChoGGi_RestoreRequestMaintenance",
+  "author", "ChoGGi",
+  "code", {
+    "Script.lua",
+  },
+})
