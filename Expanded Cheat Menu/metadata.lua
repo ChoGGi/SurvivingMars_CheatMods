1ed2e83b369c1b2318dc191c81d7d3f3191e7c8d
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-05-31 11:37:40 -0600

    Expanded Cheat Menu:
    version bump
    
    ExpandedCheatMenu@choggi.org

--- Expanded Cheat Menu/metadata.lua
+++ Expanded Cheat Menu/metadata.lua
@@ -1,7 +1,7 @@
 return PlaceObj("ModDef", {
-	"title", "Cheat Menu Expanded v4.9 Test",
+	"title", "Cheat Menu Expanded v4.9",
 	"version", 49,
-	"saved", 1527595200,
+	"saved", 1527768000,
 	"description", "\nEnables the game's built-in cheat tools, as well as adding a crapload of custom menu settings. "
     .. "There's a cheats section in most info panels on the right side of the screen."
     .. "\n\nF2: Toggle the cheats menu.."
