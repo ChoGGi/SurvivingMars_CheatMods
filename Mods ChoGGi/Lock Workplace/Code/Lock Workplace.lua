ddc31652c449a7b4b7e84a330510e5f08569cc3e
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-06-26 10:22:29 -0600

    Lock Workplace: Added a Lock Workers to workplaces.
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Lock Workplace/Code/Lock Workplace.lua
+++ Mods ChoGGi/Lock Workplace/Code/Lock Workplace.lua
@@ -1,19 +1,31 @@
 local T = LockWorkplace.ComFuncs.Trans
 local RetName = LockWorkplace.ComFuncs.RetName
 
+local string = string
+
+local PlaceObj = PlaceObj
+local XTemplates = XTemplates
+local ObjModified = ObjModified
+
+local function LoopWorkplace(context,which)
+  for i = 1, #context.workers do
+    for j = 1, #context.workers[i] do
+      local c = context.workers[i][j]
+      c.ChoGGi_Lockworkplace = which
+    end
+  end
+end
+
 function OnMsg.ClassesBuilt()
   --add button to colonists
-  local XTemplates = XTemplates
 
   if not XTemplates.ipColonist.ChoGGi_Lockworkplace then
     XTemplates.ipColonist.ChoGGi_Lockworkplace = true
 
+    --add button to colonists
     XTemplates.ipColonist[1][#XTemplates.ipColonist[1]+1] = PlaceObj("XTemplateTemplate", {
       "__context_of_kind", "Colonist",
       "__template", "InfopanelActiveSection",
---~       "Icon", "",
---~       "Title", "",
---~       "RolloverText", "",
       "RolloverTitle","",
       "RolloverHint",  "",
       "OnContextUpdate", function(self, context)
@@ -49,6 +61,46 @@ function OnMsg.ClassesBuilt()
             context.ChoGGi_Lockworkplace = nil
           else
             context.ChoGGi_Lockworkplace = true
+          end
+          ---
+          ObjModified(context)
+        end
+      })
+    })
+
+    --add button to workplaces
+    XTemplates.sectionWorkplace[#XTemplates.sectionWorkplace+1] = PlaceObj("XTemplateTemplate", {
+      "__context_of_kind", "Workplace",
+      "__template", "InfopanelActiveSection",
+      "RolloverTitle","",
+      "RolloverHint",  "",
+      "OnContextUpdate", function(self, context)
+        ---
+        if context.ChoGGi_Lockworkplace then
+          self:SetRolloverText([[Remove the lock on this workplace.]])
+          self:SetTitle([[Unlock Workers]])
+          self:SetIcon("UI/Icons/traits_approve.tga")
+        else
+          self:SetRolloverText([[Lock all workers to this workplace.]])
+          self:SetTitle([[Lock Workers]])
+          self:SetIcon("UI/Icons/traits_disapprove.tga")
+        end
+        ---
+      end,
+    }, {
+      PlaceObj("XTemplateFunc", {
+        "name", "OnActivate(self, context)",
+        "parent", function(parent, context)
+          return parent.parent
+        end,
+        "func", function(self, context)
+          ---
+          if context.ChoGGi_Lockworkplace then
+            context.ChoGGi_Lockworkplace = nil
+            LoopWorkplace(context)
+          else
+            context.ChoGGi_Lockworkplace = true
+            LoopWorkplace(context,true)
           end
           ---
           ObjModified(context)
--- Mods ChoGGi/Lock Workplace/metadata.lua
+++ Mods ChoGGi/Lock Workplace/metadata.lua
@@ -1,16 +1,14 @@
 return PlaceObj("ModDef", {
 --~ 	"image", "Preview.png",
-  "saved", 1529928000,
-  "version", 1,
-  "title", "Lock Workplace v0.1",
+  "saved", 1530014400,
+  "version", 2,
+  "title", "Lock Workplace v0.2",
   "id", "ChoGGi_LockWorkplace",
   "author", "ChoGGi",
   "steam_id", "1422914403",
   "code", {"Script.lua"},
-  "description", [[Adds a "Lock Workplace" button to the selection panel for colonists.
-They can still be fired (like if you shutdown the building), they just won't change to a new workplace.
+  "description", [[Adds a "Lock Workplace" button to the selection panel for colonists, and workplaces ("Lock Workers").
+They can still be fired (like if you shutdown the building/shift), they just won't change to a new workplace.
 
-May cause weird issues, so I didn't add a batch function to it.
-
-I can also make it so they'll never change the workplace; shutdown or not (if anyone wants that).]],
+I can also make it so they'll never change the workplace; shutdown or not (if anyone wants that), it may cause weird issues.]],
 })
