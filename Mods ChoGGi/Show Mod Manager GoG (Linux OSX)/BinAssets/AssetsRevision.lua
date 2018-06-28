30f7ad16dd75a050d57d6506c8c594bc2f8d5b84
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-06-28 05:48:25 -0600

    Show Mod Manager GoG (Linux OSX): broke in the curo update
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Show Mod Manager GoG (Linux OSX)/BinAssets/AssetsRevision.lua
+++ Mods ChoGGi/Show Mod Manager GoG (Linux OSX)/BinAssets/AssetsRevision.lua
@@ -1,17 +1,20 @@
-function OnMsg.Autorun()
 
-  local p = Platform
-  local buttons = XTemplates.PGMenu[1][2][3]
-  for i = 1, #buttons do
-    if buttons[i].ActionId == "idModManager" then
-      buttons[i].__condition = function(parent, context)
-        --return Platform.steam or Platform.pc
-        return p.steam or p.pc or p.linux or p.osx
+function OnMsg.ReloadLua()
+  CreateRealTimeThread(function()
+    local p = Platform
+    local buttons = XTemplates.PGMenu[1][2][3]
+    for i = 1, #buttons do
+      if buttons[i].ActionId == "idModManager" then
+        buttons[i].__condition = function(parent, context)
+          --return Platform.steam or Platform.pc
+          return p.steam or p.pc or p.linux or p.osx
+        end
       end
     end
-  end
+  end)
 
 end
+
 
 --return revision, or else you get a blank map on new game
 MountPack("ChoGGi_BinAssets", "Packs/BinAssets.hpk")
