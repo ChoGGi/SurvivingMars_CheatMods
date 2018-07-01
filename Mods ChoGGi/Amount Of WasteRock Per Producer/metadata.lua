67d25c8f2246be032282aca59037319ff8ad5c18
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-07-01 04:55:43 -0600

    Amount Of WasteRock Per Producer: Halves the amount of waste rock.
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Amount Of WasteRock Per Producer/metadata.lua
@@ -0,0 +1,13 @@
+return PlaceObj("ModDef", {
+  "version", 1,
+  "title", "Amount Of WasteRock Per Producer v0.1",
+  "saved", 1530446400,
+  "id", "ChoGGi_AmountOfWasteRockPerProducer",
+  "author", "ChoGGi",
+	"code", {"Script.lua"},
+--~ 	"image", "Preview.png",
+  "steam_id", "1428163993",
+  "description", [[Halves amount of waste rock each producer produces.
+
+Requested by PichuTrainer.]],
+})
