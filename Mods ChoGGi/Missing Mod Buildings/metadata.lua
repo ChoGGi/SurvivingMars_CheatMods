4ec77d8db6ef439fa52845e1e19e4a238ed3e626
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-07-17 00:26:37 -0600

    Expanded Cheat Menu: More missing mods fixes
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Missing Mod Buildings/metadata.lua
+++ Mods ChoGGi/Missing Mod Buildings/metadata.lua
@@ -10,7 +10,6 @@ return PlaceObj("ModDef", {
   "code", {"Script.lua"},
   "description", [[This replaces my Missing Residences/Missing Workplaces mods, it also adds support for some other missing buildings.
 
-
 If you installed a mod that adds certain buildings, then removed the mod without removing them; your game won't load...
 
 This fixes that, and will remove any broked buildings.
