92ecf8a2f416189a0e912ea37d6c82d2574bc7d0
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-07-19 17:05:10 -0600

    Log Flush: Flushes at a couple new places. Added commented out options for NewHour/NewMinute.
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Log Flush/Script.lua
+++ Mods ChoGGi/Log Flush/Script.lua
@@ -1,22 +1,37 @@
+local OnMsg = OnMsg
 local FlushLogFile = FlushLogFile
 
+-- early as possible
+FlushLogFile()
+
+function OnMsg.ModsLoaded()
+  FlushLogFile()
+end
+
+-- loaded a saved game
 function OnMsg.LoadGame()
   FlushLogFile()
 end
+
+-- started new game
 function OnMsg.CityStart()
   FlushLogFile()
 end
 
---and for good measure
+-- and for good measure.
 function OnMsg.ReloadLua()
   FlushLogFile()
 end
 
+-- daily flush is good for the sol.
 function OnMsg.NewDay()
   FlushLogFile()
 end
 
---i left this one commented out as it happens a bit too often
 --~ function OnMsg.NewHour()
+--~   FlushLogFile()
+--~ end
+
+--~ function OnMsg.NewMinute()
 --~   FlushLogFile()
 --~ end
--- Mods ChoGGi/Log Flush/metadata.lua
+++ Mods ChoGGi/Log Flush/metadata.lua
@@ -1,12 +1,14 @@
 return PlaceObj("ModDef", {
-	"saved", 1529150400,
-	"title", "Log Flush v0.1",
+	"title", "Log Flush v0.2",
+	"version", 2,
+	"saved", 1532001600,
 	"id", "ChoGGi_LogFlush",
 	"steam_id", "1414089790",
 	"author", "ChoGGi",
-	"version", 1,
 	"code", {"Script.lua"},
 	"image", "Preview.png",
 	"description", [[This calls the FlushLogFile() command as soon as the game loads, as well as each new Sol.
-Now if SM crashes a certain way (that doesn"t create the log), you still have a log to look at.]],
+Now if SM crashes a certain way (that doesn't create the log), you still have a log to look at.
+
+Also included are commented out options to flush on NewHour/NewMinute.]],
 })
