f174225c16dbb6dc71c467b171653a7c108fff4b
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-07-25 22:14:11 -0600

    Custom Music: Cleaned up the code a tad.
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Custom Music/items.lua
+++ Mods ChoGGi/Custom Music/items.lua
@@ -1,4 +1,3 @@
-return {
 --[[
 Place files in AppData/Surviving Mars/Music
 As far as I know it only plays opus and wav
@@ -9,32 +8,35 @@ Spaces and what not in names are a-ok
 test if music format works
 PlaySound("AppData/Music/some filename.ext","Music") --"Music", "UI"
 --]]
-PlaceObj('ModItemRadioStation', {
-	'name', "CustomMusic",
-	'display_name',"Custom Music",
-	'silence', 1, --seconds between tracks
-	'play', function (self)
-    local table = table
-    local Music,WaitMsg,Sleep,AsyncRand = Music,WaitMsg,Sleep,AsyncRand
 
-    local err, files = AsyncListFiles("AppData/Music")
-    if err or #files < 1 then
-      CreateRealTimeThread(WaitCustomPopupNotification,
-        "Help",
-        "Place files in AppData/Surviving Mars/Music.\nAs far as I know it only plays opus and wav.",
-        {"OK"}
-      )
-      return
-    end
-    while true do
-      table.permute(files, AsyncRand())           -- shuffle files list, with random seed
-      for i = 1, #files do
-        Music:PlayTrack({path = files[i]})        -- start playing one track via the music subsystem
-        WaitMsg("MusicTrackEnded", 30*60*1000)  -- wait till it ends, or at most 30 minutes
-        Sleep(self.silence * 1000)              -- pause for silence
+local table = table
+local Music,WaitMsg,Sleep,AsyncRand = Music,WaitMsg,Sleep,AsyncRand
+
+local delay_between_tracks = 1 -- seconds
+
+return {
+  PlaceObj("ModItemRadioStation", {
+    "name", "CustomMusic",
+    "display_name",[[Custom Music]],
+    "play", function (self)
+      local err, files = AsyncListFiles("AppData/Music")
+      if err or #files < 1 then
+        CreateRealTimeThread(WaitCustomPopupNotification,
+          "Help",
+          [[Place files in AppData/Surviving Mars/Music.
+As far as I know it only plays opus and wav.]],
+          {"OK"}
+        )
+        return
       end
-    end
-  end,
-}),
-
+      while true do
+        table.permute(files, AsyncRand())           -- shuffle files list, with random seed
+        for i = 1, #files do
+          Music:PlayTrack({path = files[i]})        -- start playing one track via the music subsystem
+          WaitMsg("MusicTrackEnded", 30*60*1000)  -- wait till it ends, or at most 30 minutes
+          Sleep(delay_between_tracks * 1000)              -- pause for silence
+        end
+      end
+    end,
+  })
 }
--- Mods ChoGGi/Custom Music/metadata.lua
+++ Mods ChoGGi/Custom Music/metadata.lua
@@ -1,13 +1,14 @@
 return PlaceObj("ModDef", {
+	"title", "Custom Music v0.2",
+	"version", 2,
+  "saved", 1532520000,
 	"image", "Preview.png",
-	"title", "Custom Music v0.1",
-	"description", [[Place files in AppData/Surviving Mars/Music
-As far as I know it only plays opus and wav
-mp3,aac,ogg,flac,aiff = don't work
-Spaces and what not in names are a-ok.]],
 	"tags", "Music",
 	"id", "ChoGGi_CustomMusic",
 	"author", "ChoGGi",
   "steam_id", "1411106409",
-	"version", 1,
+	"description", [[Place files in AppData/Surviving Mars/Music
+As far as I know it only plays opus and wav
+mp3,aac,ogg,flac,aiff = don't work
+Spaces and what not in names are a-ok.]],
 })
