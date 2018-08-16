7fdf0113ef4396ca0e52c5e2971ee38c3f7e627a
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-08-15 18:09:25 -0600

    Mods: Removed a mod I didn't mean to...
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Restore Request Maintenance/metadata.lua
@@ -0,0 +1,12 @@
+return PlaceObj("ModDef", {
+  "title", "Restore Request Maintenance v0.1",
+  "version", 1,
+  "saved", 1533297600,
+	"image", "Preview.png",
+  "id", "ChoGGi_RestoreRequestMaintenance",
+  "author", "ChoGGi",
+  "steam_id", "1411114444",
+	"code", {"Script.lua"},
+	"lua_revision", LuaRevision,
+  "description", [[Restores "Request Maintenance" button.]],
+})
