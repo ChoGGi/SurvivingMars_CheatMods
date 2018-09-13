101672ce197500ac7130f4b7e0a420556ff156e6
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-09-12 23:08:30 -0600

    Expanded Cheat Menu:
    Make sure Console>Examine uses the un-blacklisted _G if it can.
    
    ExpandedCheatMenu@choggi.org

--- Expanded Cheat Menu/Code/ECM_Functions.lua
+++ Expanded Cheat Menu/Code/ECM_Functions.lua
@@ -294,4 +294,29 @@ function OnMsg.ClassesGenerate()
 		end
 	end
 
+	-- if ECM is running without the bl, then we use the _G from ECM instead of the Library mod
+	if not blacklist then
+		-- "some.some.some.etc" = returns etc as object
+		function ChoGGi.ComFuncs.DotNameToObject(str,root,create)
+			-- always start with _G
+			local obj = root or _G
+			-- https://www.lua.org/pil/14.1.html
+			for name,match in str:gmatch("([%w_]+)(.?)") do
+				-- . means we're not at the end yet
+				if match == "." then
+					-- create is for adding new settings in non-existent tables
+					if not obj[name] and not create then
+						-- our treasure hunt is cut short, so return nadda
+						return
+					end
+					-- change the parent to the child (create table if absent, this'll only fire when create)
+					obj = obj[name] or {}
+				else
+					-- no more . so we return as conquering heroes with the obj
+					return obj[name]
+				end
+			end
+		end
+	end
+
 end
