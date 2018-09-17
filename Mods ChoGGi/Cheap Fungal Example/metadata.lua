74b4d259cf1c97b939c00ce49e825dad8092d8c3
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-09-17 01:34:04 -0600

    Cheap Fungal Example:
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Cheap Fungal Example/metadata.lua
@@ -0,0 +1,15 @@
+return PlaceObj("ModDef", {
+	"title", "Cheap Fungal Example v0.1",
+	"version", 1,
+	"saved", 000000000,
+	"image", "Preview.png",
+	"id", "ChoGGi_CheapFungalExample",
+--~ 	"steam_id", "000000000",
+	"author", "ChoGGi",
+	"lua_revision", LuaRevision,
+	"code", {
+		"Code/Script.lua",
+	},
+	"description", [[Makes the Fungal Farm cost less and produce more food.
+This replaces the existing farm in the build menu, but probably doesn't affect farms that already exist.]],
+})
