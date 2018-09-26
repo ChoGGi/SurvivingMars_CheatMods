95955fcdd681016c13b653e66db4af97d4e7e91e
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-09-25 20:35:01 -0600

    Logo:
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Logos/Logo/metadata.lua
@@ -0,0 +1,17 @@
+return PlaceObj("ModDef", {
+	"title", "Logo",
+	"version", 1,
+	"saved", 1537876800,
+	"image", "Preview.png",
+	"id", "Something_Oh_So_Unique",
+--~ 	"steam_id", "1520843337",
+	"author", "You",
+	"lua_revision", LuaRevision,
+	"code", {
+		"Code/Script.lua"
+	},
+	"description", [[Some Logo
+
+philriggin's image is under his copyright.
+https://steamcommunity.com/app/464920/discussions/0/1733213724915608421/]],
+})
