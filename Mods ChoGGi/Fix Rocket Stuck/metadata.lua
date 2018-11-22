b30bdfd0159a29d30ed15395e95b06fa2ee35781
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-11-21 19:45:29 -0700

    Expanded Cheat Menu:
    I broke clicking on points in examine (some of them).
    
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Fix Rocket Stuck/metadata.lua
+++ Mods ChoGGi/Fix Rocket Stuck/metadata.lua
@@ -1,7 +1,7 @@
 return PlaceObj("ModDef", {
-	"title", "Fix: Rocket Stuck v0.2",
-	"version", 2,
-	"saved", 1542715200,
+	"title", "Fix: Rocket Stuck v0.3",
+	"version", 3,
+	"saved", 1542801600,
 	"image", "Preview.png",
 	"id", "ChoGGi_FixExpeditionRocketStuckUnloading",
 	"steam_id", "1567028510",
@@ -13,5 +13,6 @@ return PlaceObj("ModDef", {
 	"description", [[This will check on load game for certain rockets stuck on the ground unable to do jack.
 So far:
 Unloading colonists.
-Maintenance */*.]],
+Maintenance */5.
+Drones stuck inside.]],
 })
