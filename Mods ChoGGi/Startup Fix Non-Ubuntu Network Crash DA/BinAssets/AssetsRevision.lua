a20cd72f7db810013b5372aed7e0219400d00a26
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-11-29 00:37:45 -0700

    Startup mods: Updated Show Mod Manager GoG (Linux OSX) cleaned up code to parity with my startup mod.
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Startup Fix Non-Ubuntu Network Crash DA/BinAssets/AssetsRevision.lua
+++ Mods ChoGGi/Startup Fix Non-Ubuntu Network Crash DA/BinAssets/AssetsRevision.lua
@@ -2,41 +2,47 @@
 dofolder_files("BinAssets/Code")
 
 CreateRealTimeThread(function()
-  -- wait till mods are loaded
-  WaitMsg("ModDefsLoaded")
+	-- wait till mods are loaded
+	WaitMsg("ModDefsLoaded")
 
-  -- build a list of ids from lua files in "Mod Ids"
-  local AsyncFileToString = AsyncFileToString
-  local mod_ids = {}
-  local err, files = AsyncListFiles("BinAssets/Mod Ids","*.lua")
-  if not err and #files > 0 then
-    for i = 1, #files do
-      local err,id = AsyncFileToString(files[i])
-      if not err then
-        mod_ids[id] = true
-      end
-    end
-  end
+	-- build a list of ids from lua files in "Mod Ids"
+	local AsyncFileToString = AsyncFileToString
+	local mod_ids = {}
+	local err, files = AsyncListFiles("BinAssets/Mod Ids","*.lua")
+	if not err and #files > 0 then
+		for i = 1, #files do
+			local err,id = AsyncFileToString(files[i])
+			if not err then
+				mod_ids[id] = true
+			end
+		end
+	end
 
-  -- remove blacklist for any mods in "Mod Ids"
-  for _,mod in pairs(Mods) do
-    if mod_ids[mod.steam_id] then
-      -- i don't set this in mod\metadata.lua so it gives an error
-      mod.lua_revision = LuaRevision
-      -- just a little overreaching with that blacklist (yeah yeah, safety first and all that)
-      mod.old_env = mod.env
-      mod.env = nil
-      -- add a warning to any mods without a blacklist, so user knows something is up
-      mod.title = table.concat{mod.title," (Warning)"}
-      mod.description = table.concat{[[Warning: The blacklist function added in the Da Vinci update has been removed for this mod!
+	-- remove blacklist for any mods in "Mod Ids"
+	local StringFormat = string.format
+	local LuaRevision = LuaRevision
+	for _,mod in pairs(Mods) do
+		if mod_ids[mod.steam_id] then
+			-- i don't set this in mod\metadata.lua so it gives an error
+			mod.lua_revision = LuaRevision
+			-- save the old env somewhere, CurrentModPath maybe
+			mod.env_old = mod.env
+			-- just a little overreaching with that blacklist (yeah yeah, safety first and all that)
+			mod.env = nil
+			-- add a warning to any mods without a blacklist, so user knows something is up
+			mod.title = StringFormat("%s (Warning)",mod.title)
+			mod.description = StringFormat([[Warning: The blacklist function added in the Da Vinci update has been removed for this mod!
 This means it has no limitations and can access your Steam name, Friends list, and any files on your computer.
 In other words, the same as Curiosity and lower.
 
-]],mod.description}
-    end
-  end
+%s]],mod.description)
+		end
+	end
+
+	-- just in case anything needs it
+	Msg("ChoGGi_Blacklist")
 end)
 
 -- return revision, or else you get a blank map on new game
 MountPack("ChoGGi_BinAssets", "Packs/BinAssets.hpk")
-return dofile("ChoGGi_BinAssets/AssetsRevision.lua") or 233360
+return dofile("ChoGGi_BinAssets/AssetsRevision.lua") or 235636
--- Mods ChoGGi/Startup Show Mod Manager GoG (Linux OSX)/BinAssets/Code/Show Mod Manager GoG (Linux OSX).lua
+++ Mods ChoGGi/Startup Show Mod Manager GoG (Linux OSX)/BinAssets/Code/Show Mod Manager GoG (Linux OSX).lua
@@ -1,21 +1,59 @@
+local Sleep = Sleep
+local TableFind = table.find
 
-function OnMsg.ReloadLua()
+local function SetModNew(menu)
+	local buttons = menu.idContent and menu.idContent.idBottomButtons
+	local idx = TableFind(buttons,"class","XWindow")
+
+	if idx then
+		buttons = buttons[idx]
+		buttons.idModManager = XWindow:new({
+			Id = "idModManager",
+			ZOrder = 0,
+		}, buttons)
+
+		XTextButton:new({
+			Text = T{1129, --[[XTemplate PGMenuNew ActionName]] "MOD MANAGER"},
+			Background = 16777215,
+			DisabledBackground = 7895160,
+			FocusedBackground = 16777215,
+			FXMouseIn = "MainMenuItemHover",
+			FXPress = "MainMenuItemClick",
+			FXPressDisabled = "UIDisabledButtonPressed",
+			MouseCursor = "UI/Cursors/Rollover.tga",
+			PressedBackground = 16777215,
+			RolloverAnchor = "center-top",
+			RolloverBackground = 16777215,
+			RolloverTemplate = "Rollover",
+			TextStyle = "Action",
+			Translate = true,
+			VAlign = "center",
+			OnPress = function()
+				menu:SetMode("ModManager")
+			end,
+		}, buttons.idModManager)
+
+	end
+end
+
+function OnMsg.DesktopCreated()
 
   CreateRealTimeThread(function()
-		-- loop through menu items till we find the correct one
-		local buttons = XTemplates.PGMenu[1][2][3]
-		local TableFind = table.find
-		for i = 1, #buttons do
+		local Dialogs = Dialogs
 
-			local idx = TableFind(buttons[i],"ActionId","idModManager")
-			if idx then
-				buttons[i][idx].__condition = function()
-					return true
-				end
-				break
-			end
-
+		while not Dialogs.PGMainMenu do
+			Sleep(100)
 		end
+
+		local orig_SetMode = Dialogs.PGMainMenu.SetMode
+		Dialogs.PGMainMenu.SetMode = function(self,...)
+			orig_SetMode(self,...)
+			SetModNew(self)
+		end
+
+		-- fire the first time
+		SetModNew(Dialogs.PGMainMenu)
+
 	end)
 
 end
