bd48f550fa175a7726a3e0af1696300b4c7aaec4
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2018-12-03 01:21:49 -0700

    Mods: Wrong dds file for the example
    ExpandedCheatMenu@choggi.org

--- Mods ChoGGi/Example New Res Icon/metadata.lua
+++ Mods ChoGGi/Example New Res Icon/metadata.lua
@@ -1,10 +1,10 @@
 return PlaceObj("ModDef", {
-	"title", "Example New Res Icon v0.1",
+	"title", "Example New Res Icons v0.1",
 	"version", 1,
 	"saved", 1543492800,
 	"image", "Preview.png",
-	"id", "ChoGGi_ExampleNewResIcon",
---~ 	"steam_id", "000000000",
+	"id", "ChoGGi_ExampleNewResIcons",
+	"steam_id", "1581373413",
 	"author", "ChoGGi & Rusty",
 	"lua_revision", LuaRevision,
 	"code", {
@@ -16,6 +16,8 @@ Includes instructions for image files to entity files.
 Known Issues:
 The textures will be darkened till you zoom in (once the shader is loaded it's fine till you quit the game).
 if anyone can figure out a way around... That'd be nice to know.
+
+https://github.com/ChoGGi/SurvivingMars_CheatMods/tree/master/Mods%20ChoGGi/Example%20New%20Res%20Icon
 
 A huge thank you to Rusty for the annoying .mtl file futzing around.]],
 })
