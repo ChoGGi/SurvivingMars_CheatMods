f895130a37fd5140b51b47ceb5dab436ab312410
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2019-02-08 08:49:32 -0700

    Expanded Cheat Menu:
    - Widened width of mod upload dialog to fit all checkboxes.
    
    License 2018-2019
    
    ExpandedCheatMenu@choggi.org

--- ChoGGi's Library/Code/Init.lua
+++ ChoGGi's Library/Code/Init.lua
@@ -1,3 +1,5 @@
+-- See LICENSE for terms
+
 local LICENSE = [[
 Any code from https://github.com/HaemimontGames/SurvivingMars is copyright by their LICENSE
 
@@ -5,7 +7,7 @@ All of my code is licensed under the MIT License as follows:
 
 MIT License
 
-Copyright (c) [2018] [ChoGGi]
+Copyright (c) [2019] [ChoGGi]
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -146,7 +148,7 @@ ChoGGi = {
 	lang = GetLanguage(),
 	-- path to this mods' folder
 	library_path = CurrentModPath,
-	-- i translate all my strings at startup (and a couple of the built-in ones)
+	-- stores my strings, and edited sm ones
 	Strings = false,
 	-- easier access to some data (traits,cargo,mysteries,colonist data)
 	Tables = false,
@@ -195,10 +197,10 @@ ChoGGi = {
 local ChoGGi = ChoGGi
 
 do -- translate
-	local locale_path = ChoGGi.library_path .. "Locales/%s.csv"
+	local locale_path = ChoGGi.library_path .. "Locales/"
 	-- load locale translation (if any, not likely with the amount of text, but maybe a partial one)
-	if not LoadTranslationTableFile(locale_path:format(GetLanguage())) then
-		LoadTranslationTableFile(locale_path:format("English"))
+	if not LoadTranslationTableFile(locale_path .. GetLanguage() .. ".csv") then
+		LoadTranslationTableFile(locale_path .. "English.csv")
 	end
 
 	Msg("TranslationChanged")
