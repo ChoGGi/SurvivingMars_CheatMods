428edd974545bb064620db4a8b0b9f91ae7dcb64
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2019-04-17 02:28:42 -0600

    Mods: Medical Sanatorium Medical Centre that acts like a Sanatorium.
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Medical Sanatorium/Code/Script.lua
@@ -0,0 +1,73 @@
+-- See LICENSE for terms
+
+DefineClass.MedicalSanatorium = {
+  __parents = {
+    "Sanatorium",
+    "InteriorAmbientLife"
+  },
+  flags = {efWalkable = true},
+  interior = {
+    "InfirmaryInterior"
+  },
+
+}
+
+function OnMsg.ClassesPostprocess()
+	if not BuildingTemplates.MedicalSanatorium then
+		PlaceObj("BuildingTemplate",{
+			"Id", "MedicalSanatorium",
+			"template_class", "MedicalSanatorium",
+			"dome_required",true,
+
+			"construction_cost_Concrete", 30000,
+			"construction_cost_Metals", 20000,
+			"construction_cost_Polymers", 10000,
+			"maintenance_resource_type", "Polymers",
+			"maintenance_resource_amount", 2000,
+
+			"build_points", 30000,
+			"build_category", "Dome Services",
+
+			"upgrade1_id", "Sanatorium_BehavioralMelding",
+			"upgrade1_display_name", T(5243, "Behavioral Melding"),
+			"upgrade1_description", T(5244, "Replaces flaws with Perks for visitors."),
+			"upgrade1_icon", "UI/Icons/Upgrades/behavioral_melding_01.tga",
+			"upgrade1_upgrade_cost_Polymers", 10000,
+			"upgrade1_upgrade_cost_Electronics", 10000,
+
+			"display_name", "Medical Sanatorium",
+			"display_name_pl", "Medical Sanatoriums",
+			"description", T(5246, "Treats Colonists for flaws through advanced and (mostly) humane medical practices."),
+			"display_icon", "UI/Icons/Buildings/sanatorium.tga",
+
+			"encyclopedia_id", "Sanatorium",
+			"encyclopedia_image", "UI/Encyclopedia/Sanatorium.tga",
+
+			"demolish_sinking", range(8, 18),
+			"demolish_tilt_angle", range(600, 900),
+			"demolish_debris", 80,
+			"entity", "Infirmary",
+			"palette_color1", "inside_base",
+			"palette_color2", "inside_accent_medical",
+			"palette_color3", "inside_accent_service",
+			"palette_color4", "inside_metal",
+
+			"entity2", "InfirmaryCP3",
+			"entitydlc2", "contentpack3",
+			"palette2_color1", "inside_accent_medical",
+			"palette2_color2", "inside_base",
+			"palette2_color3", "inside_accent_service",
+			"palette2_color4", "inside_metal",
+			"label1", "InsideBuildings",
+
+			"electricity_consumption", 20000,
+			"enabled_shift_1", false,
+			"enabled_shift_3", false,
+			"max_visitors", 4,
+			"evaluation_points", 200,
+			"trait1", "Gambler",
+			"trait2", "Alcoholic",
+			"trait3", "Glutton"
+		})
+	end
+end
---
+++ Mods ChoGGi/Medical Sanatorium/metadata.lua
@@ -0,0 +1,19 @@
+return PlaceObj("ModDef", {
+	"title", "Medical Sanatorium",
+	"version_major", 0,
+	"version_minor", 1,
+	"saved", 1555416000,
+	"image", "Preview.png",
+	"id", "ChoGGi_MedicalSanatorium",
+	"steam_id", "1714882321",
+	"pops_any_uuid", "dcdfc9a6-a551-4c76-89a1-fb596c71373c",
+	"author", "ChoGGi",
+	"lua_revision", LuaRevision or 244275,
+	"code", {
+		"Code/Script.lua",
+	},
+	"description", [[Medical Centre that acts like a Sanatorium.
+
+
+Requested by Tassyr.]],
+})
