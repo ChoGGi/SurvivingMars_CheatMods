8fc8f4c77b5aeba969aa95cca72c0ca8b946639a
ChoGGi <gitmsg.txt ExpandedCheatMenu@choggi.org>
2019-05-17 13:39:54 -0600

    Expanded Cheat Menu:
    - Re-added Add Water Each Sol (thanks windows).
    
    ExpandedCheatMenu@choggi.org

---
+++ Mods ChoGGi/Add Water Each Sol/metadata.lua
@@ -0,0 +1,22 @@
+return PlaceObj("ModDef", {
+	"title", "Add Water Each Sol",
+	"version", 5,
+	"version_major", 0,
+	"version_minor", 5,
+	"saved", 1536062400,
+	"id", "ChoGGi_AddWaterEachSol",
+	"author", "ChoGGi",
+	"image", "Preview.png",
+	"steam_id", "1440164001",
+	"pops_any_uuid", "370cf581-9210-435e-8457-1d2685195ce8",
+	"lua_revision", 244677,
+	"code", {
+		"Code/Script.lua",
+	},
+	"has_options", true,
+	"description", [[Adds 50 units of water to any visible deposits each new Sol.
+
+This will not increase the capacity, so if the deposit is full that's it.
+
+You can adjust the amount added with mod options.]],
+})
