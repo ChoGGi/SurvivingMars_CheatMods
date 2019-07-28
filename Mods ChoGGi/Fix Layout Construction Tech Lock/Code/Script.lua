87df496dfedba971f930b9b43b7d2b15e86d8c84
ChoGGi <github@choggi.org>
2019-07-27 21:02:53 -0600

    Mods:
    Fix Layout Construction Tech Lock:
    Layout construction allows building buildings that should be locked by tech (Triboelectric Scrubber).
    
    github@choggi.org

---
+++ Mods ChoGGi/Fix Layout Construction Tech Lock/Code/Script.lua
@@ -0,0 +1,27 @@
+-- See LICENSE for terms
+
+local GetBuildingTechsStatus = GetBuildingTechsStatus
+
+local orig_Activate = LayoutConstructionController.Activate
+function LayoutConstructionController:Activate(...)
+	orig_Activate(self, ...)
+
+	local UICity = UICity
+	local BuildingTemplates = BuildingTemplates
+
+	local controllers = self.controllers or empty_table
+	for entry, controller in pairs(controllers) do
+		local template = BuildingTemplates[entry.template]
+		if template then
+			local tech_shown, tech_enabled = GetBuildingTechsStatus(template.id, template.build_category)
+
+			-- if it isn't unlocked and there's no prefabs then remove it
+			if not tech_enabled and UICity:GetPrefabs(template.id) == 0 then
+				self.skip_items[entry] = true
+				controllers[entry]:Deactivate()
+				controllers[entry] = nil
+			end
+
+		end
+	end
+end
---
+++ Mods ChoGGi/Fix Layout Construction Tech Lock/metadata.lua
@@ -0,0 +1,18 @@
+return PlaceObj("ModDef", {
+	"title", "Fix Layout Construction Tech Lock",
+	"version", 1,
+	"version_major", 0,
+	"version_minor", 1,
+	"saved", 0,
+	"image", "Preview.png",
+	"id", "ChoGGi_FixLayoutConstructionTechLock",
+	"steam_id", "1817485290",
+	"pops_any_uuid", "efea6b39-a260-4885-8a33-27e190b0b441",
+	"author", "ChoGGi",
+	"lua_revision", 245618,
+	"code", {
+		"Code/Script.lua",
+	},
+	"TagOther", true,
+	"description", [[Layout construction allows building buildings that should be locked by tech (Triboelectric Scrubber).]],
+})
