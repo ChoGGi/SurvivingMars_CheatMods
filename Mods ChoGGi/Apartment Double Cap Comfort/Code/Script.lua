8ce61e7c3b64f4463c9f77792b75cc1d3556b518
ChoGGi <github@choggi.org>
2021-06-28 20:13:25 -0600

    Mods:
    Updated example mod.
    
    github@choggi.org

--- Mods ChoGGi/Apartment Double Cap Comfort/Code/Script.lua
+++ Mods ChoGGi/Apartment Double Cap Comfort/Code/Script.lua
@@ -3,10 +3,11 @@
 local capacity
 local service_comfort
 
+-- update the templates for newly built buildings
 function OnMsg.ClassesPostprocess()
 	local a = BuildingTemplates.Apartments
 
-	-- we need values for LoadGame
+	-- we need values for OnMsg.LoadGame
 	capacity = a.capacity * 2
 	service_comfort = a.service_comfort * 2
 
@@ -20,21 +21,22 @@ function OnMsg.ClassesPostprocess()
 
 end
 
--- so it only updates once per game
 GlobalVar("g_ChoGGi_ApartmentDoubleCapComfort", false)
 
 -- this will update the settings for existing apartments
 function OnMsg.LoadGame()
-	if g_ChoGGi_ApartmentDoubleCapComfort then
-		return
-	end
 
-	local objs = UICity.labels.Apartments or ""
-	for i = 1, #objs do
-		local obj = objs[i]
-		obj.capacity = capacity
-		obj.service_comfort = service_comfort
+	-- so the below only happens once per game (you can do it every load, but that'll take more time the more apartments).
+	if not g_ChoGGi_ApartmentDoubleCapComfort then
+		-- loop through all existing buildings and update them
+		local objs = UICity.labels.Apartments or ""
+		for i = 1, #objs do
+			local obj = objs[i]
+			obj.capacity = capacity
+			obj.service_comfort = service_comfort
+		end
 	end
-
+	-- update var kept in savefile
 	g_ChoGGi_ApartmentDoubleCapComfort = true
+
 end
