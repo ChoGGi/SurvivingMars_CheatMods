9508487accdc86e45a16126453a62db141e763c4
ChoGGi <github@choggi.org>
2022-01-02 17:32:59 -0700

    Mods:
    Fix Sol 2983:
    - Whoops
    
    github@choggi.org

--- Mods ChoGGi/Fix Sol 2983/Code/Script.lua
+++ Mods ChoGGi/Fix Sol 2983/Code/Script.lua
@@ -3,10 +3,10 @@
 -- 2147483647 is 2982 Sols 14h, but for some reason GameTime() doesn't reach it till 2983 20h
 -- can't be bothered to find out why...
 
--- used below to check for rollover
+-- used below to check for overflow (we want it below 2981 since that's when I start testing)
 local min_time = 2980 * const.Scale.sols
--- 2147483647
-local max_int = max_int
+-- 2147483647 * 2
+local max_int2 = max_int * 2
 
 local mod_EnableMod
 
@@ -23,22 +23,24 @@ OnMsg.ModsReloaded = ModOptions
 -- Fired when Mod Options>Apply button is clicked
 OnMsg.ApplyModOptions = ModOptions
 
--- when true add max_int time
+-- when true add max_int2 time
 local add_time
 
 -- There's other stuff to fix, but for now this will fix most of it
 local ChoOrig_GameTime = GameTime
 function GameTime()
-	if not mod_EnableMod or not add_time then
-		return ChoOrig_GameTime()
+	if mod_EnableMod and add_time then
+		return ChoOrig_GameTime() + max_int2
 	end
-	return ChoOrig_GameTime() + max_int
+	return ChoOrig_GameTime()
 end
 
-
+-- when min_time is larger than GameTime then we've overflowed
 local function TestTime()
-	if UIColony.day > 2981 and min_time > ChoOrig_GameTime() then
-		add_time = true
+	if not add_time then
+		if UIColony.day > 2981 and min_time > ChoOrig_GameTime() then
+			add_time = true
+		end
 	end
 end
 
