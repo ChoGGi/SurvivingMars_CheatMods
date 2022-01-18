04e0cb8c05a7415725102401d9995fe6b2607d3a
ChoGGi <github@choggi.org>
2022-01-17 20:33:24 -0700

    Mods:
    Allow BB Buildings 0.3:
    Forgot about class templates?
    
    #50
    
    github@choggi.org

--- Mods ChoGGi/Allow BB Buildings/Code/Script.lua
+++ Mods ChoGGi/Allow BB Buildings/Code/Script.lua
@@ -2,20 +2,29 @@
 
 local mod_EnableMod
 
+--~ function ChoGGi.ComFuncs.SetBuildingTemplates(template, key, value)
+local function SetBuildingTemplates(template, key, value, bt, ct)
+	bt[template][key] = value
+	ct[template][key] = value
+end
+
 local function UnlockBuildings()
 	if not mod_EnableMod then
 		return
 	end
 
+	local ClassTemplates_Building = ClassTemplates.Building
 	local BuildingTemplates = BuildingTemplates
 	for _, template in pairs(BuildingTemplates) do
-		template.disabled_in_environment1 = ""
-		template.disabled_in_environment2 = ""
-		template.disabled_in_environment3 = ""
-		template.disabled_in_environment4 = ""
+		SetBuildingTemplates(template, "disabled_in_environment1", "", BuildingTemplates, ClassTemplates_Building)
+		SetBuildingTemplates(template, "disabled_in_environment2", "", BuildingTemplates, ClassTemplates_Building)
+		SetBuildingTemplates(template, "disabled_in_environment3", "", BuildingTemplates, ClassTemplates_Building)
+		SetBuildingTemplates(template, "disabled_in_environment4", "", BuildingTemplates, ClassTemplates_Building)
 	end
 
 end
+OnMsg.CityStart = UnlockBuildings
+OnMsg.LoadGame = UnlockBuildings
 
 local function ModOptions(id)
 	-- id is from ApplyModOptions
@@ -36,7 +45,3 @@ end
 OnMsg.ModsReloaded = ModOptions
 -- fired when Mod Options>Apply button is clicked
 OnMsg.ApplyModOptions = ModOptions
-
-
-OnMsg.CityStart = UnlockBuildings
-OnMsg.LoadGame = UnlockBuildings
--- Mods ChoGGi/Allow BB Buildings/metadata.lua
+++ Mods ChoGGi/Allow BB Buildings/metadata.lua
@@ -4,9 +4,9 @@ return PlaceObj("ModDef", {
 	"steam_id", "2595725610",
 	"pops_any_uuid", "72724cbe-731f-4edd-b7bb-fcabaa1b10ec",
 	"lua_revision", 1007000, -- Picard
-	"version", 2,
+	"version", 3,
 	"version_major", 0,
-	"version_minor", 2,
+	"version_minor", 3,
 	"image", "Preview.jpg",
 	"author", "ChoGGi",
 	"code", {
